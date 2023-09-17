bb8c873172bb59b0578be9f5344ad87add6c5a1a
roberto <lozano.roberto.2018@gmail.com>
2023-09-17 18:48:44 -0500

    changes on app component

--- angular-ci-cd-test/src/app/app.component.spec.ts
+++ angular-ci-cd-test/src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.title')?.textContent).toContain('Titulo Ready!');
+    expect(compiled.querySelector('.title')?.textContent).toContain('otra cosa');
   });
 });
